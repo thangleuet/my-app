0f61227e73ab92970cbde340508ede8d8bdfbdcd
Lê Tất Thắng <84216929+thangleuet@users.noreply.github.com>
2021-06-07 21:30:03 +0700

    Add api https://jsonplaceholder.typicode.com/todos

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -3,27 +3,10 @@ import TodoItems from './TodoItems';
 import AddTodo from './AddTodo';
 import axios from 'axios'
 
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
 
 function Todos() {
-    const [todos, setTodos] = useState([
-        {
-            id: uuidv4(),
-            title: "Việc 1",
-            complete: false,
-        },
-        {
-            id: 2,
-            title: "Việc 2",
-            complete: false,
-        },
-        {
-            id: 3,
-            title: "Việc 3",
-            complete: false,
-        },
-
-    ]);
+    const [todos, setTodos] = useState([]);
 useEffect(
     () => {
         const getTodo = async () =>{
@@ -39,7 +22,9 @@ useEffect(
         getTodo()
     },[]
 )
-    const setTicked = (id) => {
+    const setTicked = async (id) => {
+
+    
         const newTodo = todos.map(todo => {
             if (todo.id === id) todo.complete = !todo.complete;
             return todo;
@@ -48,19 +33,30 @@ useEffect(
         setTodos(newTodo);
     }
 
-    const deleteTodo = (id) => {
-        const newTodo = todos.filter(todo => {
-            return todo.id !== id;
-        })
-        setTodos(newTodo);
+    const deleteTodo = async (id) => {
+       try {
+         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        
+       } catch (error) {
+           console.log(error.massage)
+       }
+       const newTodos = todos.filter(todo =>{
+        return todo.id !==id;
+    })
+    setTodos(newTodos)
     }
-    const AddTodos = title => {
-        const newTodo = [...todos, 
-            {   id: Math.random(), 
-                title: title, 
-                complete:false
-            }]
-        setTodos (newTodo);
+    const AddTodos = async (title) => {
+        try {
+            const res = await axios.post('https://jsonplaceholder.typicode.com/todos',{
+                title: title,
+                complete : false
+            })
+            const newTodos = [...todos, res.data]
+            setTodos (newTodos);
+        } catch (error) {
+            console.log(error.massage)
+        }
+
     }
     return (
         <>
